b901456d84f70d92e9a4557c71f7fc30a8e0efa9
Md. Abdullah-Al-Bakee <mail.bakee@gmail.com>
2018-07-25 17:32:35 +0600

    Add Windows Form Application in Visual Studio 2017.

---
+++ app/windows/BluetoothRemote/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace BluetoothRemote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonBackward = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.Location = new System.Drawing.Point(114, 129);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(75, 53);
+            this.buttonForward.TabIndex = 0;
+            this.buttonForward.Text = "Forward";
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.buttonForward_Click);
+            this.buttonForward.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // buttonBackward
+            // 
+            this.buttonBackward.Location = new System.Drawing.Point(114, 300);
+            this.buttonBackward.Name = "buttonBackward";
+            this.buttonBackward.Size = new System.Drawing.Size(75, 53);
+            this.buttonBackward.TabIndex = 0;
+            this.buttonBackward.Text = "Backward";
+            this.buttonBackward.UseVisualStyleBackColor = true;
+            this.buttonBackward.Click += new System.EventHandler(this.buttonBackward_Click);
+            this.buttonBackward.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(114, 219);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 53);
+            this.buttonStop.TabIndex = 0;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            this.buttonStop.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(224, 219);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(75, 53);
+            this.buttonRight.TabIndex = 0;
+            this.buttonRight.Text = "Right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            this.buttonRight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(7, 219);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(75, 53);
+            this.buttonLeft.TabIndex = 0;
+            this.buttonLeft.Text = "Left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            this.buttonLeft.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.Location = new System.Drawing.Point(7, 374);
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(302, 45);
+            this.trackBarSpeed.TabIndex = 1;
+            this.trackBarSpeed.Scroll += new System.EventHandler(this.trackBarSpeed_Scroll);
+            this.trackBarSpeed.ValueChanged += new System.EventHandler(this.trackBarSpeed_ValueChanged);
+            this.trackBarSpeed.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(67, 6);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(173, 21);
+            this.comboBoxPorts.TabIndex = 2;
+            this.comboBoxPorts.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Devices:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(306, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(246, 6);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 5;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            this.buttonRefresh.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 422);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.buttonBackward);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonForward);
+            this.Name = "Form1";
+            this.Text = "Bluetooth Car Controller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonBackward;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonRefresh;
+    }
+}
+
---
+++ app/windows/BluetoothRemote/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace BluetoothRemote
+{
+    public partial class Form1 : Form
+    {
+        int _currentSpeed = 100;
+        private SerialPort _serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBarSpeed_Scroll(object sender, EventArgs e)
+        {
+        }
+
+        private void ReloadPorts()
+        {
+            comboBoxPorts.Items.Clear();
+            var availablePorts = SerialPort.GetPortNames();
+
+            comboBoxPorts.Items.AddRange(availablePorts);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReloadPorts();
+            EnableControls(false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text == "Connect")
+            {
+                handleConnect();
+            }
+            else
+            {
+                handleDisconnect();
+            }
+        }
+
+        private void EnableControls(bool enable)
+        {
+            buttonForward.Enabled = enable;
+            buttonBackward.Enabled = enable;
+            buttonLeft.Enabled = enable;
+            buttonRight.Enabled = enable;
+            buttonStop.Enabled = enable;
+            trackBarSpeed.Enabled = enable;
+
+            comboBoxPorts.Enabled = !enable;
+            buttonRefresh.Enabled = !enable;
+        }
+
+        private void handleConnect()
+        {
+            if (comboBoxPorts.SelectedItem == null || String.IsNullOrEmpty(comboBoxPorts.SelectedItem.ToString()))
+            {
+                MessageBox.Show("Please select a port first", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var portName = comboBoxPorts.SelectedItem.ToString();
+            _serialPort = new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
+            _serialPort.Open();
+            button1.Text = "DisConnect";
+            EnableControls(true);
+        }
+
+        private void handleDisconnect()
+        {
+            _serialPort.Close();
+            button1.Text = "Connect";
+            EnableControls(false);
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            ReloadPorts();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Stop();
+            if (_serialPort != null && _serialPort.IsOpen)
+            {
+                _serialPort.Close();
+            }
+        }
+
+        private void Stop()
+        {
+            if (_serialPort == null || !_serialPort.IsOpen) return;
+            _serialPort.Write("d0\n");
+        }
+
+        private void Forward()
+        {
+            if (_serialPort == null || !_serialPort.IsOpen) return;
+            _serialPort.Write("d1\n");
+        }
+
+        private void Backward()
+        {
+            if (_serialPort == null || !_serialPort.IsOpen) return;
+            _serialPort.Write("d2\n");
+        }
+
+        private void Left()
+        {
+            if (_serialPort == null || !_serialPort.IsOpen) return;
+            _serialPort.Write("d3\n");
+        }
+
+        private void Right()
+        {
+            if (_serialPort == null || !_serialPort.IsOpen) return;
+            _serialPort.Write("d4\n");
+        }
+
+        private void SetSpeed(int speed)
+        {
+            if(speed > 100)
+            {
+                speed = 100;
+            }
+
+            if(speed < 0)
+            {
+                speed = 0;
+            }
+
+            _currentSpeed = speed;
+
+            if(trackBarSpeed.Value * 10 != _currentSpeed)
+            {
+                trackBarSpeed.Value = _currentSpeed / 10;
+            }
+
+            if (_serialPort == null || !_serialPort.IsOpen) return;
+            var speedString = String.Format("s{0}\n", speed);
+            _serialPort.Write(speedString);
+        }
+
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+            Forward();
+        }
+
+        private void buttonBackward_Click(object sender, EventArgs e)
+        {
+            Backward();
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            Left();
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            Right();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void trackBarSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            var newValue = trackBarSpeed.Value;
+            newValue *= 10;
+            SetSpeed(newValue);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 32)
+            {
+                Stop();
+            }
+            this.Focus();
+            e.Handled = true;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            //capture up arrow key
+            if (keyData == Keys.Up)
+            {
+                Forward();
+                return true;
+            }
+            //capture down arrow key
+            if (keyData == Keys.Down)
+            {
+                Backward();
+                return true;
+            }
+            //capture left arrow key
+            if (keyData == Keys.Left)
+            {
+                Left();
+                return true;
+            }
+            //capture right arrow key
+            if (keyData == Keys.Right)
+            {
+                Right();
+                return true;
+            }
+            //capture right arrow key
+            if (keyData == Keys.Add)
+            {
+                if(_currentSpeed < 100)
+                {
+                    _currentSpeed += 10;
+                }
+                SetSpeed(_currentSpeed);
+                return true;
+            }
+            //capture right arrow key
+            if (keyData == Keys.Subtract)
+            {
+                if (_currentSpeed > 0)
+                {
+                    _currentSpeed -= 10;
+                }
+                SetSpeed(_currentSpeed);
+                return true;
+            }            
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
